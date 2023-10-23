055762885ee31b63bfe4fb92f8ce6dc2e468058d
Ahmed Saqr <ahmedhassansaqr28@gmail.com>
2023-10-23 18:16:28 +0300

    new changes

---
+++ lib/Modules/New Document/newDocumentScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewDocumentScreen extends StatelessWidget {
+  const NewDocumentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+import 'Bloc Observer/blocObserver.dart';
+import 'Modules/Main Menu/mainMenuScreen.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+            size: 25.0,
+          ),
+        ),
+      ),
+      home: const MainMenuScreen(),
+    );
+  }
+}
